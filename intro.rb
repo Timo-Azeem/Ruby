b61218b05fc8f4c05e9dac850c150db79a751343
Timo-Azeem <azeem.reich2017@gmail.com>
2018-03-05 22:11:55 +0200

    initial step to Ruby

---
+++ intro.rb
@@ -0,0 +1,21 @@
+# some calculation
+
+puts 80 / 10
+puts 10 - 4
+puts 3 + 4
+puts 1 * 5
+puts 5 - 2
+puts 1 - 1
+puts 3 * 3
+
+
+# something happened
+
+puts 3
+puts 1
+puts 4
+puts 1
+puts 5
+puts 9
+
+# there is no error
